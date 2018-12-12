2b6d244829e5ba09e60eed64ebb9828136652e1e
visualkernel <visualkernel@163.com>
2018-12-12 15:08:03 +0800

    remove 'elpa' directory

--- elpa/json-reformat-20160212.53/json-reformat-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; json-reformat-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "json-reformat" "json-reformat.el" (22596 65141
-;;;;;;  283663 187000))
-;;; Generated autoloads from json-reformat.el
-
-(autoload 'json-reformat-region "json-reformat" "\
-Reformat the JSON in the specified region.
-
-If you want to customize the reformat style,
-please see the documentation of `json-reformat:indent-width'
-and `json-reformat:pretty-string?'.
-
-\(fn BEGIN END)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; json-reformat-autoloads.el ends here
--- elpa/material-theme-20160908.1538/material-theme-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; material-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "material-light-theme" "material-light-theme.el"
-;;;;;;  (22647 7475 730565 283000))
-;;; Generated autoloads from material-light-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil "material-theme" "material-theme.el" (22647
-;;;;;;  7475 662564 187000))
-;;; Generated autoloads from material-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("material-theme-pkg.el") (22647 7475 889534
-;;;;;;  966000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; material-theme-autoloads.el ends here
