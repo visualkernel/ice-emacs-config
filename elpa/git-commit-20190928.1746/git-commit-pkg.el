c2bf7eb0d0ecfdf5d283463a83115fe7289e7003
visualkernel <visualkernel@163.com>
2019-12-23 13:30:52 +0800

    upgrade packages

--- elpa/git-commit-20190928.1746/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190928.1746" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "62dad50a4669066cb66f9c4003c6cc93fe3b1380" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- init.el
+++ init.el
@@ -53,17 +53,3 @@
 (install-pkg swiper)
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (swiper slime magit hungry-delete highlight-parentheses company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
